e6c539f02092c5f7b865175e62cab6b47cd1b695
howrugit <howrugit@cntv.cn>
2017-03-22 10:06:17 +0800

    add dns.java

---
+++ app/src/main/java/c/b/a/myapplication_600/dns.java
@@ -0,0 +1,13 @@
+package c.b.a.myapplication_600;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dns);
+    }
+}
