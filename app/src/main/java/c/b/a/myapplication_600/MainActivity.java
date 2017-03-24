3b4720221b862798842539d8e86bdc16b476e143
howrugit <howrugit@cntv.cn>
2017-03-24 15:28:38 +0800

    2

--- app/src/main/java/c/b/a/myapplication_600/MainActivity.java
+++ app/src/main/java/c/b/a/myapplication_600/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getxml();
         action();
     }
